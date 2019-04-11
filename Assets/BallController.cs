79f8caadaa9a84688a9a2eb795a7eee12a4e966f
Yuto Michishita <y-michishita@i-enter.co.jp>
2019-04-11 11:28:03 +0900

    PinBallプロジェクト　初めてのコミット lesson5を見ながら作成したファイルです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	private float visiblePosZ = -6.5f;
+	private int Score;
+	public int sumScore;
+	private GameObject gameoverText;
+	private GameObject ScoreText;
+	// Use this for initialization
+	void Start () {
+		this.gameoverText = GameObject.Find ("GameOverText");
+		this.ScoreText = GameObject.Find ("ScoreText");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.z < this.visiblePosZ) {
+
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+
+		}
+		Score = GameScore (0);
+		this.ScoreText.GetComponent<Text> ().text = "Score :"+Score;
+	}
+
+	public int GameScore(int score){
+		
+		sumScore = sumScore + score;
+		return sumScore;
+	}
+}
---
+++ Assets/BrightnessRegulator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessRegulator : MonoBehaviour {
+
+	Material myMaterial;
+
+	private float minEmission = 0.3f;
+
+	private float magEmission = 2.0f;
+
+	private int degree = 0;
+
+	private int speed = 10;
+
+	private int Score1;
+
+	private BallController ballController;
+
+	Color defaultColor = Color.white;
+	// Use this for initialization
+	void Start () {
+
+		if (tag == "SmallStarTag") {
+			this.defaultColor = Color.white;
+			Score1 = 20;
+		} else if (tag == "LargeStarTag") {
+			this.defaultColor = Color.white;
+			Score1 = 30;
+
+		} else if (tag == "SmallCloudTag" || tag == "LargeCloudTag") {
+			this.defaultColor = Color.cyan;
+			Score1 = 40;
+		}
+		this.myMaterial = GetComponent<Renderer> ().material;
+
+		myMaterial.SetColor ("_EmissionColor", this.defaultColor * minEmission);
+			
+		ballController = GameObject.Find ("Ball").GetComponent<BallController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.degree >= 0) {
+			Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin (this.degree * Mathf.Deg2Rad) * this.magEmission);
+			myMaterial.SetColor ("_EmissionColor", emissionColor);
+			this.degree -= this.speed;
+		}
+	}
+	void OnCollisionEnter(Collision other){
+		this.degree = 180;
+		ballController.GameScore (Score1);
+		}
+}
